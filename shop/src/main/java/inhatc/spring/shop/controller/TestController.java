fb84973d99f0aedabbfbd656b40dd7ee1ad709ee
RODONG <jsw26637368@gmail.com>
2023-09-05 23:24:48 +0900

    init5

--- shop/src/main/java/inhatc/spring/shop/controller/TestController.java
+++ shop/src/main/java/inhatc/spring/shop/controller/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
     public PersonDto hello(){
         PersonDto p = new PersonDto();
         p.setAge(10);
-        p.setName("김미네");
+        p.setName("영상이");
         log.info("p : " + p);
         return p;
     }
